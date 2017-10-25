c0e7f6a63ea902bafcced7340a9b1239a33051cc
Grace Dai <gracedai@Graces-MacBook-Air.local>
2017-10-24 23:01:55 -0400

    Fixed some details that didn't cost me points, will fix the cost pointing triggering methods that I messed up on now

---
+++ src/planning/SortTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class SortTest {
+	
+	ArrayList<Assignment> testAssignmentList = new ArrayList<>();
+	ArrayList<Dependency> testDependencyList = new ArrayList<>();
+	
+	@Test
+	public void creating_new_assignments_test() {	
+		
+		assertTrue(true);
+		
+	}
+	
+
+	
+	
+
+}
