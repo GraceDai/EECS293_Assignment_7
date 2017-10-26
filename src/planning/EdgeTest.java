9178ddb03d6cb4380b43307c68a78f4ca7c8b8ab
Grace Dai <gracedai@Graces-MacBook-Air.local>
2017-10-26 19:40:23 -0400

    finished testing my assignment and edge class testing

---
+++ src/planning/EdgeTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EdgeTest {
+
+	/*
+	 * getFromNode -- no test
+	 */
+	
+	/*
+	 * getToNode -- no test
+	 */
+	
+	/*
+	 * getDuration -- no test
+	 */
+
+}
